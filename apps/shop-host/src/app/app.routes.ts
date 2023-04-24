4ef1ee6fcbe6b40fbce4384518bfe0165b0c3cac
qperior <konrad.nowacki@q-perior.com>
2023-04-24 17:50:06 +0200

    feat(navbar): Add basic navbar view

--- apps/shop-host/src/app/app.routes.ts
+++ apps/shop-host/src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Route } from '@angular/router';
-import {AppComponent} from "./app.component";
+import { AppComponent } from './app.component';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appRoutes: Route[] = [
   {
     path: 'products',
-    loadChildren: () => import('shop-all-products/Routes').then((m) => m.remoteRoutes),
+    loadChildren: () =>
+      import('shop-all-products/Routes').then((m) => m.remoteRoutes),
   },
   {
     path: '',
     component: AppComponent,
+    providers: [provideStore(), provideEffects()],
   },
 ];
