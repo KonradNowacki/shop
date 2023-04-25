d9373c8552c1da606751fd1f5c6b11174e220b0d
qperior <konrad.nowacki@q-perior.com>
2023-04-25 19:25:20 +0200

    refactor(general): remove microfrontends

---
+++ libs/shop-client-lib/src/lib/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'shop-home',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>home works!</p>`,
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {}
